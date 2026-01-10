e27928b5f89c0af53e94b3be923db5cf210b56d3
eliaquinn <eliaquin.j.araujo@gmail.com>
2026-01-09 23:43:04 -0400

    update formmatter

--- nvim/.config/nvim/lua/eliaquinn/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/eliaquinn/plugins/lsp/lspconfig.lua
@@ -71,19 +71,35 @@ return {
 
 				-- Lua LSP
 				vim.lsp.config.lua_ls = {
+					cmd = { "lua-language-server" },
+					filetypes = { "lua" },
+					root_markers = {
+						".luarc.json",
+						".luarc.jsonc",
+						".luacheckrc",
+						".stylua.toml",
+						"stylua.toml",
+						"selene.toml",
+						"selene.yml",
+						".git",
+					},
 					capabilities = capabilities,
 					on_attach = on_attach,
 					settings = {
 						Lua = {
+							diagnostics = {
+								globals = { "vim" },
+							},
 							completion = {
 								callSnippet = "Replace",
 							},
-							diagnostics = { globals = { "vim" } },
 							workspace = {
-								checkThirdParty = false,
 								library = vim.api.nvim_get_runtime_file("", true),
+								checkThirdParty = false,
 							},
-							telemetry = { enable = false },
+							telemetry = {
+								enable = false,
+							},
 						},
 					},
 				}
