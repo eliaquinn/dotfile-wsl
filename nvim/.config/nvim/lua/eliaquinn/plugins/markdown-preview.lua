1ae07c822c73d940637900bb9fb1787dbf134307
eliaquinn <eliaquin.j.araujo@gmail.com>
2026-03-02 18:46:05 -0400

    merged branch dbview with main

--- nvim/.config/nvim/lua/eliaquinn/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/eliaquinn/plugins/markdown-preview.lua
@@ -1,20 +1,18 @@
 return {
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = "cd app && npm install",
-    keys = {
-      { "gm", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" },
-    },
-    config = function()
-      vim.g.mkdp_auto_close = true
-      vim.g.mkdp_open_to_the_world = false -- Listen on localhost only
-      vim.g.mkdp_open_ip = "127.0.0.1"     -- Use localhost
-      vim.g.mkdp_browser = ""              -- Use default system browser
-      vim.g.mkdp_echo_preview_url = true   -- Echo the URL in the command line
-    end,
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreview", "MarkdownPreviewStop" },
+  ft = { "markdown" },
+  build = "cd app && npm install",
+  keys = {
+    { "gm", "<cmd>MarkdownPreviewToggle<cr>", desc = "Markdown Preview" },
   },
+  config = function()
+    vim.g.mkdp_auto_close = true
+    vim.g.mkdp_open_to_the_world = false -- Listen on localhost only
+    vim.g.mkdp_open_ip = "127.0.0.1"     -- Use localhost
+    vim.g.mkdp_browser = ""              -- Use default system browser
+    vim.g.mkdp_echo_preview_url = true   -- Echo the URL in the command line
+  end,
 }
 
 
