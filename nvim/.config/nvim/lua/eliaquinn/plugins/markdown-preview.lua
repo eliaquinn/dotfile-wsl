967030bd11e9feee6537e0fb3dfd78c9add2908d
eliaquinn <eliaquin.j.araujo@gmail.com>
2026-03-03 17:29:44 -0400

    markdown-preview.lua and tmux.conf has been updated

--- nvim/.config/nvim/lua/eliaquinn/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/eliaquinn/plugins/markdown-preview.lua
@@ -10,11 +10,10 @@ return {
     vim.g.mkdp_auto_close = true
     vim.g.mkdp_open_to_the_world = false -- Listen on localhost only
     vim.g.mkdp_open_ip = "127.0.0.1"     -- Use localhost
-    vim.g.mkdp_browser = ""              -- Use default system browser
+    vim.g.mkdp_browser = "wslview"       -- Use wslview (WSL-compatible browser launcher)
     vim.g.mkdp_echo_preview_url = true   -- Echo the URL in the command line
   end,
 }
-
 
 -- return {
 --   {
